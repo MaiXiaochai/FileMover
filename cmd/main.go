89085c4c7d4c0987e16c350b877163ccc65299bb
MaiXiaochai <>
2024-03-25 17:07:00 +0800

    基本完成，文件路径待修改

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"file_mover/pkg/mover"
+	"log"
+	"os"
+	"time"
+)
 
+// 定义条件函数的类型
+type conditionFunc func(file os.FileInfo) bool
+
+// 实现一个具体的条件函数
+var isTimeSafe conditionFunc = func(file os.FileInfo) bool {
+	// 如果文件的修改日期比当前时间大两小时
+	now := time.Now()
+	twoHoursLater := now.Add(2 * time.Hour).Unix()
+	return file.ModTime().Unix() <= twoHoursLater
+}
+
+func main() {
+	srcDir := ""
+	destDir := ""
+	err := mover.MoveFiles(srcDir, destDir, isTimeSafe)
+	if err != nil {
+		log.Printf("Failed to move files: %v", err)
+	}
+	log.Println("Files moved successfully.")
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module file_mover
+
+go 1.22.1
